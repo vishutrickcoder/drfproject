6ad41d3563612c1f731a73d3a41e93ba31591e96
Vishu <vs1015242@gmail.com>
2022-12-31 17:08:48 +0530

    updates

--- drfproject/settings.py
+++ drfproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g(%&+cx-w2r!b$nne7yh7x9numd^@q*d%vyxtbhxj%#jgenzzb'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'movies.apps.MoviesConfig',
 ]
 
 MIDDLEWARE = [
